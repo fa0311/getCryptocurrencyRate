7b42573808a8ca7c442d55cc04034bf6ea3883b7
nanato12 <admin@nanato12.info>
2021-02-28 04:59:16 +0900

    Refactoring

--- getCryptocurrencyRate.py
+++ getCryptocurrencyRate.py
@@ -1,62 +1,50 @@
-import urllib.parse
-import urllib.request
 import json
+from dataclasses import dataclass
+from urllib import parse, request
+
+BASE_URL = "https://bitflyer.com/api/app/market/line_chart"
 
 
-class getCryptocurrencyRate:
-    def __init__(self, product_code='BTC_JPY', scale='hour'):
-        self.url = 'https://bitflyer.com/api/app/market/line_chart'
-        self.product_code = product_code
+class CryptoCurrencyRate:
+    def __init__(self, currency: str = "BTC_JPY", scale: str = "hour"):
+        self.currency = currency
         self.scale = scale
 
     def get(self):
-        param = {
-            'product_code': self.product_code,
-            'scale': self.scale
-        }
-        paramStr = urllib.parse.urlencode(param)
-        readObj = urllib.request.urlopen(self.url + "?" + paramStr)
-        response = readObj.read()
-        data = json.loads(response)
-        return self.parse(data)
+        query: str = parse.urlencode(
+            {"product_code": self.currency, "scale": self.scale}
+        )
+        data: bytes = request.urlopen(f"{BASE_URL}?{query}").read()
+        json_data: dict = json.loads(data)
+        return CurrencyData(**json_data)
 
-    class parse:
-        def __init__(self, data):
-            self.data = data
 
-        def status(self):
-            return self.data["status"]
+@dataclass
+class CurrencyData:
+    data: dict = None
+    status: int = None
+    message: str = None
+    errors: str = None
 
-        def label(self):
-            return self.data["label"]
+    @property
+    def change_str(self):
+        return self.data["change_str"]
 
-        def message(self):
-            return self.data["message"]
+    @property
+    def change_str(self):
+        return self.data["change_str"]
 
-        def errors(self):
-            return self.data["errors"]
+    @property
+    def change(self):
+        return self.data["change"]
 
-        def change_str(self):
-            return self.data["data"]["change_str"]
+    @property
+    def price_info_list(self):
+        return [PriceInfo(**price) for price in self.data["prices"]]
 
-        def change(self):
-            return self.data["data"]["change"]
 
-        def prices_list(self):
-            return self.data["data"]["prices"]
-
-        def prices(self, i):
-            return self.parse(self.data["data"]["prices"][i])
-
-        class parse:
-            def __init__(self, data):
-                self.data = data
-
-            def price(self):
-                return self.data["price"]
-
-            def price_str(self):
-                return self.data["price_str"]
-
-            def timestamp(self):
-                return self.data["timestamp"]
+@dataclass
+class PriceInfo:
+    price: float
+    price_str: str
+    timestamp: int
--- sample.py
+++ sample.py
@@ -1,18 +1,19 @@
-from getCryptocurrencyRate import getCryptocurrencyRate
 from datetime import datetime
+
+from getCryptocurrencyRate import CryptoCurrencyRate
 
 product = "BTC_JPY"
 # ["BTC_JPY", "XRP_JPY", "ETH_JPY", "XTZ_JPY", "XLM_JPY", "XEM_JPY", "BAT_JPY", "ETC_JPY", "LTC_JPY", "BCH_JPY", "MONA_JPY", "LSK_JPY"]
 scale = "hour"
 # ["hour","day","week","month","year"]
 
-res = getCryptocurrencyRate(product, scale).get()
+res = CryptoCurrencyRate(product, scale).get()
 
 print("\n***情報***")
-print("リクエストステータス " + str(res.status()))
-print("現在 " + res.prices(-1).price_str() + "JPY")
-print("推移 " + res.change_str() + "%")
+print("リクエストステータス " + str(res.status))
+print("現在 " + res.prices_list[-1].price_str + "JPY")
+print("推移 " + res.change_str + "%")
 print("\n***一覧***")
-for i in range(len(res.prices_list())):
-    print(datetime.fromtimestamp(res.prices(i).timestamp()))
-    print(res.prices(i).price_str() + "JPY")
+for price_info in res.price_info_list:
+    print(datetime.fromtimestamp(price_info.timestamp))
+    print(price_info.price_str + "JPY")
